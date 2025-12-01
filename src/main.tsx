18f770f6e8329017b6662f484fd41813856a3d2e
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-12-01 23:23:33 +0300

    ekzamsв

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-// import { App } from "@/app/App"
-// import { createRoot } from "react-dom/client"
-// import "./index.css"
-// import { Provider } from "react-redux"
-// import { BrowserRouter } from "react-router"
-// import { store } from "./app/store"
-//
-// createRoot(document.getElementById("root")!).render(
-//     <BrowserRouter>
-//         <Provider store={store}>
-//             <App />
-//         </Provider>
-//     </BrowserRouter>,
-// )
-// import { createRoot } from "react-dom/client"
-//
+import { App } from "@/app/App"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router"
+import { store } from "./app/store"
+
+
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
+)
+//import { createRoot } from "react-dom/client"
+
 // export const App = () => {
 //     return (
 //         <div>
