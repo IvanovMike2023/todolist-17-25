d65d8d4230dab484a07490d79269319407d968ee
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-12-02 18:07:47 +0300

    s

--- src/app/Child.tsx
+++
@@ -1,9 +0,0 @@
-import React, {memo} from "react";
-
-export const Child=React.memo((props:any)=>{
-    console.log('Child')
-    console.log(props)
-    return (
-        <div>c11115</div>
-    )
-})
