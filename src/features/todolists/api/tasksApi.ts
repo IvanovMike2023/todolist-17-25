cf99f761acd209366546a7cd71ad2104de4b7c45
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-01-19 22:59:53 +0300

    invalidatesTags

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -7,56 +7,51 @@ export type TasksState = Record<string, DomainTask[]>
 
 
 export const tasksApi = baseApi.injectEndpoints({
-endpoints: (build) => ({
-    getTask: build.query<GetTasksResponse, {todolistId:string,params:{count:number,page:number}}>({
-      query: ({todolistId,params}) => {
-          return {
-              url: `/todo-lists/${todolistId}/tasks`,params
-          }
-      },
-       providesTags:(res, err,id)=>
-        res ? res.items.map((task) => {
-               return {type: 'Tasks', id: task.id}
-           }) : ['Tasks']
-
-           //return [{type: 'Tasks', id: nanoid()}]
-
-           //? [...res.items.map(({ id }) => ({ type: "Tasks", id }) as const), { type: "Tasks", id: todolistId }]
-            //['Tasks']
+    endpoints: (build) => ({
+        getTask: build.query<GetTasksResponse, { todolistId: string, params: { count: number, page: number } }>({
+            query: ({todolistId, params}) => {
+                return {
+                    url: `/todo-lists/${todolistId}/tasks`, params
+                }
+            },
+            providesTags: (res, err, {todolistId}) =>
+                [{type: 'Tasks', id: todolistId},]
+        }),
+        updateTasksItem: build.mutation<void, { todolistId: string, taskId: string, model: UpdateTaskModel }>({
+            query: (data) => {
+                const {todolistId, taskId, model} = data
+                return {
+                    url: `/todo-lists/${todolistId}/tasks/${taskId}`,
+                    method: 'PUT',
+                    body: model
+                }
+            },
+            invalidatesTags: (_res, _err, {todolistId}) =>
+                [{type: 'Tasks', id: todolistId}]
+        }),
+        createTask: build.mutation<void, { todolistId: string, title: string }>({
+            query: ({todolistId, title}) => {
+                return {
+                    url: `/todo-lists/${todolistId}/tasks`,
+                    method: 'post',
+                    body: {title}
+                }
+            },
+            invalidatesTags: (_res, _err, {todolistId}) =>
+                [{type: 'Tasks', id: todolistId}]
+        }),
+        deleteTask: build.mutation<void, { todolistId: string, taskId: string }>({
+            query: ({todolistId, taskId}) => ({url: `/todo-lists/${todolistId}/tasks/${taskId}`, method: 'delete'}),
+            invalidatesTags: (_res, _err, arg) => [{type: 'Tasks', id: arg.todolistId}]
+        })
     }),
-    updateTasksItem: build.mutation<void, {todolistId:string,taskId:string,model: UpdateTaskModel }>({
-      query: (data) => {
-        const {todolistId,taskId,model}=data
-        return {
-          url: `/todo-lists/${todolistId}/tasks/${taskId}`,
-          method:'PUT',
-          body:model
-        }
-      },
-        invalidatesTags: ['Tasks']//(res,err,{taskId})=> [{type:'Tasks',id:taskId}],
-    }),
-    createTask: build.mutation<void,{todolistId:string,title:string}>({
-      query: ({todolistId, title}) => {
-        return {
-          url: `/todo-lists/${todolistId}/tasks`,
-          method: 'post',
-          body: {title}
-        }
-      },
-        invalidatesTags:['Tasks'] //(res,err,{todolistId})=> [{type:'Tasks', id:todolistId }],
-    }),
-    deleteTask: build.mutation<void,{todolistId:string,taskId:string}>({
-      query: ({todolistId, taskId}) => ({url: `/todo-lists/${todolistId}/tasks/${taskId}`, method: 'delete'}),
-      invalidatesTags: ['Tasks']//(res,err,{taskId})=> [{type:'Tasks',id:taskId}],
-    })
-  }),
 
 })
 
 export const {
-  useGetTaskQuery,
-  useDeleteTaskMutation,
-  useCreateTaskMutation,
-  useUpdateTasksItemMutation
+    useGetTaskQuery,
+    useDeleteTaskMutation,
+    useCreateTaskMutation,
+    useUpdateTasksItemMutation
 } = tasksApi
 
