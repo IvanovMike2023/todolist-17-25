882392b5ac4422241347e3d6456d58a978b496b0
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-04-07 17:50:26 +0300

    DndContext SortableCon

--- src/features/todolists/ui/Todolists/TodolistSkeleton/TodolistSkeleton.tsx
+++ src/features/todolists/ui/Todolists/TodolistSkeleton/TodolistSkeleton.tsx
@@ -4,8 +4,6 @@ import Paper from "@mui/material/Paper"
 import Skeleton from "@mui/material/Skeleton"
 import styles from "./TodolistSkeleton.module.css"
 
-
-
 export const TodolistSkeleton = () => (
     <Paper className={styles.container}>
         <div className={styles.title}>
