1879ae1ff7212c36c6cdf30550783f97b0cfb66a
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-04-16 23:36:58 +0300

    getPokemon('bulbasaur')xa

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { store } from "./app/store"
 import {App} from "@/app/App";
 
 
+
+
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <Provider store={store}>
